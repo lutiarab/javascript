49ca883f1ba14175ea1909c93de9353b7ab16e44
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-04-11 10:42:00 -0300

    nova mudança

---
+++ condicionais/exercicio.2.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo no ${pais}`)
+if(pais == 'Brasil'){
+    console.log('você é Brasileiro')
+}
+else{
+    console.log('Você é Estrangeiro')
+}
---
+++ condicionais/exercicios.js
@@ -0,0 +1,4 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}`)
+console.log('Dirija sempre usando o cinto de segurança')
+console.log(' Você ultrapassou a velocidade permitida, MULTADO')
