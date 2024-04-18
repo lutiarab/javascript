127e0726fe71d2d30816176430d84be15d1f04d0
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-04-18 09:45:05 -0300

    nova mudança

---
+++ repeticoes/exercico1.1.js
@@ -0,0 +1,12 @@
+//console.log('Tem fatia?')
+//var c = 1
+/*do {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}while (c <= 8)*/
+//------------for-----------------
+console.log('Início')
+for(var c = 1; c <= 10; c++ ){
+    console.log(c)
+}
+console.log('Fim!')
